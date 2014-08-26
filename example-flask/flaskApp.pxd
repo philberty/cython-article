79dba3aeeb851b008a10b3872d79739985429ef4
redbrain <philip.herron@instil.co>
2014-08-26 12:52:38 +0100

    cleanup examples

--- example-flask/flaskApp.pxd
+++
@@ -1,6 +0,0 @@
-cdef extern from "data.h":
-    struct data:
-        char name[10]
-        int value
-    data * getData ()
-
--- example-flask/flaskApp.pyx
+++ example-flask/flaskApp.pyx
@@ -1,17 +1,19 @@
-cimport flaskApp
-from flask import Flask, jsonify
-app = Flask ('myapp')
+cdef extern from "data.h":
+    struct data:
+        char name[10]
+        int value
+    data * getData ()
 
 cpdef getBackendData():
     cdef data * resp = getData ()
     return {'name': resp.name, 'value': resp.value}
+
+from flask import Flask, jsonify
+app = Flask ('myapp')
 
 @app.route("/")
 def index ():
     return jsonify (getBackendData ())
 
 cdef public void startFlaskApp ():
-    try:
-        app.run ()
-    except KeyboardInterrupt:
-        pass
+    app.run ()
